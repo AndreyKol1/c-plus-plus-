241a9ffc1dda728a2c3dadd9a4707ec7ec2da614
Andrii <88890052+AndreyKol1@users.noreply.github.com>
2024-06-18 18:05:57 +0200

    Delete BaseButton.h

--- BaseButton.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-class BaseButton
-{
-public:
-	bool isMouseOverRec(sf::RenderWindow& window, sf::RectangleShape button);
-	bool isMouseOverCirc(sf::RenderWindow& window, sf::CircleShape button);
-};
-
